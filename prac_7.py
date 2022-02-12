05a0e436be12823e594cfb00131bf66b007d217e
yashvikotadiya04 <96778187+yashvikotadiya04@users.noreply.github.com>
2022-02-12 19:19:05 +0530

    Add files via upload

---
+++ prac_7.py
@@ -0,0 +1,19 @@
+#ID: 20CE045
+#NAME: YASHVI KOTADIYA
+#Github repository link:
+
+T = int(input())                #number of test cases
+for i in range(T):
+    n = input()                 #take string from user
+    l = len(n)                  #length of string
+    if l % 2 == 0:              #check whether the length is divisible by 2 or not
+        x = n[:l//2]            #slicing
+        y = n[l//2:]            #slicing
+    else:
+        x = n[:l//2]            #slicing
+        y = n[l//2+1:]          #slicing
+
+    if sorted(x) == sorted(y):  #compare the two sorted string
+        print("YES")
+    else:
+        print("NO")
